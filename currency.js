217f17a460a3a9efa04190769483bed8f4a7b7a6
Bitna Kim <tallshe108@hanmail.net>
2019-09-17 14:03:34 +0700

    finish milestone2

--- currency.js
+++ currency.js
@@ -4,4 +4,10 @@ function convertUsdToVnd(usd) {
   return usd * exchangeRate;
 }
 
+function convertVndToUsd(vnd){
+  return vnd/exchangeRate
+}
+
 console.log(convertUsdToVnd(500));
+console.log(convertVndToUsd(200000));
+let amountVnd= 200000
